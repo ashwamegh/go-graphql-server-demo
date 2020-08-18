aacc1fedd28a3ed91fb05926d31fbda4c4852700
Shashank Shekhar <shashank7200@gmail.com>
2020-08-18 18:16:59 +0530

    added logic to store data in mysql database

---
+++ internal/links/links.go
@@ -0,0 +1,37 @@
+package links
+
+import (
+	"log"
+
+	database "github.com/glyphack/go-graphql-server-demo/internal/pkg/db/mysql"
+	"github.com/glyphack/go-graphql-server-demo/internal/users"
+)
+
+// #1
+type Link struct {
+	ID      string
+	Title   string
+	Address string
+	User    *users.User
+}
+
+//#2
+func (link Link) Save() int64 {
+	//#3
+	stmt, err := database.Db.Prepare("INSERT INTO Links(Title,Address) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//#4
+	res, err := stmt.Exec(link.Title, link.Address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//#5
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal("Error:", err.Error())
+	}
+	log.Print("Row inserted!")
+	return id
+}

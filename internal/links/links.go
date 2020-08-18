aff731dfdf35f5ed5ff9e030018f54ffa7a3550c
Shashank Shekhar <shashank7200@gmail.com>
2020-08-18 21:20:15 +0530

    added getall function to fetch links

--- internal/links/links.go
+++ internal/links/links.go
@@ -35,3 +35,29 @@ func (link Link) Save() int64 {
 	log.Print("Row inserted!")
 	return id
 }
+
+func GetAll() []Link {
+	stmt, err := database.Db.Prepare("select id, title, address from Links")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var links []Link
+	for rows.Next() {
+		var link Link
+		err := rows.Scan(&link.ID, &link.Title, &link.Address)
+		if err != nil {
+			log.Fatal(err)
+		}
+		links = append(links, link)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return links
+}
